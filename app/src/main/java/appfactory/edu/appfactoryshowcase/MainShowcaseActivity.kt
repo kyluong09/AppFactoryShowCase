029555363a64a202279da8d840eaf05990603419
elliottcrifasi <elliottcrifasidesign@gmail.com>
2018-03-09 16:30:35 -0600

    Initial Commit. Need Carousel View dependency.

---
+++ app/src/main/java/appfactory/edu/appfactoryshowcase/MainShowcaseActivity.kt
@@ -0,0 +1,12 @@
+package appfactory.edu.appfactoryshowcase
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainShowcaseActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_showcase)
+    }
+}
